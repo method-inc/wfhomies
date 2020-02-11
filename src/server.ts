1f837017a7096842d8037a52ad7e9584134b4c57
Cody Yantis <cody_yantis@skookum.com>
2020-02-11 15:15:07 -0700

    removes mongo import alias

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import { MongoClient } as db from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 dotenv.config();
 
