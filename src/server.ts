3e4dc2ef514d29173a27a2a77b6881c5e1cdd897
Cody Yantis <cody_yantis@skookum.com>
2020-02-12 06:12:22 -0700

    adds if/else conditional back for  more accurate res parsing

--- src/server.ts
+++ src/server.ts
@@ -20,39 +20,34 @@ if (!slackToken) {
 const port = PORT || 8080;
 
 app.post('/', (req, res) => {
-    console.log('RESPONSE', res)
+    console.log('RESPONSE', res);
     // const result = commandParser(req.body);
-    switch (req.body.text) {
-        case 'home':
-            res.json({
-                response_type: 'ephemeral',
-                text: ':house_with_garden: I\'ve marked you as working from home. Enjoy your day!',
-            });
-            break;
-        case 'office':
-            res.json({
-                response_type: 'ephemeral',
-                text: ':office: I\'ve marked you as working from the office. Have fun!',
-            });
-            break;
-        case 'remote':
-            res.json({
-                response_type: 'ephemeral',
-                text: ':snowboarder: I\'ve marked you as working remotely. Wherever you are, hope you\'re well!',
-            });
-            break;
-        case 'everyone':
-            res.json({
-                response_type: 'ephemeral',
-                text: ':skookum: This will eventually return a list of where everyone is working from today',
-            });
-            break;
-        default:
-            res.json({
-                response_type: 'ephemeral',
-                text: ':question: Sorry, I didn\'t get that. Try `/wf home`, `/wf office`, `/wf remote`, or `/wf everyone`.',
-            });
-            break;
+
+    if (req.body.text.includes('home')) {
+        res.json({
+            response_type: 'ephemeral',
+            text: ':house_with_garden: I\'ve marked you as working from home. Enjoy your day!',
+        });
+    } else if (req.body.text.includes('office')) {
+        res.json({
+            response_type: 'ephemeral',
+            text: ':office: I\'ve marked you as working from the office. Have fun!',
+        });
+    } else if (req.body.text.includes('remote')) {
+        res.json({
+            response_type: 'ephemeral',
+            text: ':snowboarder: I\'ve marked you as working remotely. Wherever you are, hope you\'re well!',
+        });
+    } else if (req.body.text.includes('everyone')) {
+        res.json({
+            response_type: 'ephemeral',
+            text: ':skookum: This will eventually return a list of where everyone is working from today',
+        });
+    } else {
+        res.json({
+            response_type: 'ephemeral',
+            text: ':question: Sorry, I didn\'t get that. Try `/wf home`, `/wf office`, `/wf remote`, or `/wf everyone`.',
+        });
     }
 });
 
